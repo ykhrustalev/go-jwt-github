5b7e7d47691ebe00fc8728717820c4b4515f6a38
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2017-04-20 07:34:56 +0100

    scheme

---
+++ db/scheme.sql
@@ -0,0 +1,13 @@
+CREATE TABLE github_user_details (
+  id        SERIAL PRIMARY KEY,
+  github_id INT  NOT NULL UNIQUE,
+  info      JSON NOT NULL,
+  emails    JSON NOT NULL
+);
+
+CREATE TABLE github_tokens (
+  id             SERIAL PRIMARY KEY,
+  github_user_id INTEGER REFERENCES github_user_details (id),
+  value          TEXT NOT NULL
+);
+
