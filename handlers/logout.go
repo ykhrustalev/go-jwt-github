cb9bf4afd870626bee7dcefce9fd28907ab51e9b
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2017-04-21 06:20:51 +0100

    logout using post only

--- handlers/logout.go
+++ handlers/logout.go
@@ -10,6 +10,11 @@ import (
 
 func CreateLogoutHandler(db *pg.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "POST" {
+			jsonhttp.ErrorResponse(w, "use POST", nil, 400)
+			return
+		}
+
 		claims := req.Context().Value("claims").(*jwtutils.Claims)
 
 		err := dao.DeleteJwtToken(db, claims.TokenId)
